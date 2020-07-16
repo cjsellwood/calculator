4df9f810f500bc394448c337397da4cc299ada18
cjsellwood <c.sellwood22@gmail.com>
2020-07-16 22:33:04 +1000

    Layout for calculator done, design nearly finished.

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// notes
+// Clicking equals while the last press was operator should do nothing/ alert user somehow
+// Round decimals with long answers
+// Clear wipes all user data
+// Display error if divided by 0
+// Only allow one dot per number - disable decimal button until operator
+// After equals pressed, display input on top line, and answer on bottom,
+//      then allow another operator to be used following answer, or cleared if number pressed
+// top row goes off left side of screen when full

e2ed9dbf57baab8290a3d3eab137a14993fa392d
cjsellwood <c.sellwood22@gmail.com>
2020-07-18 17:46:24 +1000

    Screen updates done, no calculations yet.

--- scripts.js
+++ scripts.js
@@ -12,7 +12,6 @@ let screen1Text = "";
 let screen2Text = "";
 
 
-
 // Update the Screen in appropriate manner so that calculations can be performed on it
 function updateScreen(text) {
     // Get screen elements
@@ -21,23 +20,77 @@ function updateScreen(text) {
 
     // Set operators
     let operators = [" + ", " x ", " ÷ "]
+    
+    // If last pressed was equals 
+    //console.log(screen1Text)
+    let splitText = screen1Text.split(' ');
 
-    // split string on spaces for better control over screen display
-    let splitText = screen1Text.split(" ");
-    for (let i = 0)
-    console.log(splitText)
+    //console.log("_" + screen1Text + "_")
+    if (screen1Text[screen1Text.length - 2] === "=") {
+        console.log("____=____")
+        if (text === " = ") {
+            // Do nothing 
+        
+        // If button is other operator use previous answer as first number
+        } else if (operators.indexOf(text) !== -1) {
+            screen1Text = screen2Text + text;
+            screen2Text = ""
+        
+        // If clear entry pressed after equals clear answer and remove equals
+        } else if (text === "CE") {
+            //screen2Text = "";
+            screen2Text = splitText[splitText.length - 3]
+        
+        // Else clear both screens
+        } else {
+            screen1Text = "";
+            screen2Text = "";
+        }
+    }
 
     // Handle DEL button
-    if (text === "CE") {
-        if (screen1Text.slice(screen1Text.length - 1) === " ") {
-            // Delete space gaps if operator was used
-            screen1Text = screen1Text.slice(0, screen1Text.length - 3);
-            screen2Text = screen2Text.slice(0, screen2Text.length - 3);
+     if (text === "CE") {
+        let numbersOnly = splitText;
+        console.log(numbersOnly)
+        
+        // Clear any non numbers from end of numbers list previously entered
+        const unwanted = ["-", "+", "x", "÷", ""];
+        for (let i = 0; i < unwanted.length; i ++) {
+            for (let j = numbersOnly.length; j >= 0; j--) {
+                if (unwanted[i] === numbersOnly[j]) {
+                    numbersOnly.splice(j, 1);
+                }
+            }
+        }
+
+        console.log(numbersOnly)
+
+        // If the last character was negative sign, delete just it
+        if (screen1Text[screen1Text.length - 1] === "-") {
+            screen1Text = screen1Text.slice(0, screen1Text.length - 1);
+            console.log("negative sign delete")
+            screen2Text = ""
 
         } else {
-            screen1Text = screen1Text.slice(0, screen1Text.length - 1);
-            screen2Text = screen2Text.slice(0, screen2Text.length - 1);
+            let lastNumber = numbersOnly[numbersOnly.length - 1]
+            
+            //console.log("before " + screen1Text + "_")
+
+            // if operator used last delete last 3 characters
+            if (screen1Text[screen1Text.length - 3] == " " && screen1Text[screen1Text.length - 1] == " ") {
+                screen1Text = screen1Text = screen1Text.slice(0, screen1Text.length - 3);
+                screen2Text = lastNumber
+            } else {
+                screen1Text = screen1Text.slice(0, screen1Text.length - 1);
+                lastNumber = lastNumber.slice(0, lastNumber.length - 1);
+                console.log(lastNumber)
+                screen2Text = lastNumber;
+            }
+
+            //console.log("after " + screen1Text + "_")
         }
+        
+
     // Handle AC button
     } else if (text === "AC") {
         screen1Text = "";
@@ -45,6 +98,9 @@ function updateScreen(text) {
       
     // Handle decimal, allow only one per number
     } else if (text === ".") {
+        // Split string on spaces for better control over screen display
+        let splitText = screen1Text.split(" ");
+
         // If their is not a decimal in the last number entered allow adding it
         if (splitText[splitText.length - 1].indexOf(text) === -1) {
             screen1Text += text;
@@ -53,38 +109,102 @@ function updateScreen(text) {
 
     // Handle subtract/negative
     } else if (text === " - ") {
+        // Allow negative numbers at start
+        if (screen1Text === "") {
+            screen1Text += "-";
+            screen2Text += "-";
+
         // Allow negative numbers
-        if (screen2Text === "") {
-            screen1Text += text.slice(1);
-            screen2Text += text.slice(1);
-        } else if (splitText[splitText.length - 1] !== "") {
+        } else if (screen1Text[screen1Text.length - 1] === " ") {
+            screen1Text += "-";
+            screen2Text += "-";
+
+        // Disallow more than two negative in a row
+        } else if (splitText[splitText.length - 1] === "-" && splitText[splitText.length - 1] === "-") {
+            // Do nothing
+        
+        // Use as operator
+        } else if (screen2Text !== "-") {
             screen1Text += text;
             screen2Text = "";
         }
-
+    
     // Handle other operators
     } else if (operators.indexOf(text) !== -1) {
-        // If no text on screen
+        // If no text on screen don't allow operator
         if (screen1Text === "") {
+            // Do nothing
 
-        } else if (splitText[splitText.length - 1] === "") {
+        // If last input was operator don't allow another
+        } else if (screen1Text[screen1Text.length - 1] === " ") {
+            // Do nothing
+
+        // If last input was negative for number
+        } else if (screen1Text[screen1Text.length - 1] === "-") {
+            // Do nothing
+
+        // If last 
 
         } else {
             screen1Text += text;
             screen2Text = "";
         }
-        
-        
-    
 
+    // Handle equals button
+    } else if (text === " = ") {
+        // Don't allow as first text on screen
+        if (screen1Text === "") {
+            // Do nothing
+
+        // Don't allow two equals sign in a row
+        } else if (screen1Text[screen1Text.length - 1] === " ") {
+            // Do nothing
+
+        // Do nothing if last input was a negative sign
+        } else if (screen1Text[screen1Text.length - 1] === "-") {
+            // Do nothing
+
+        // Don't allow after opening bracket
+        } else if (screen1Text[screen1Text.length - 1] === "(") {
+            // Do nothing
+        } else {
+            screen1Text += text;
+
+            // Perform calculation on screen1Text
+            screen2Text = 123456789;
+        }
+    
+    } else if (text === "(") {
+        screen1Text += text;
+        screen2Text += text;
+    
+    // Disallow closing bracket if no corresponding opening bracket
+    } else if (text === ")") {
+        let openCount = 0;
+        let closedCount = 0;
+        for (let i = 0; i < screen1Text.length; i++) {
+            if (screen1Text[i] === "(") {
+                openCount += 1;
+            }
+            if (screen1Text[i] === ")") {
+                closedCount += 1;
+            }
+        }
+        if (openCount > closedCount) {
+            screen1Text += text;
+            screen2Text += text;
+        }
+
+        // Don't allow if no opening bracket
+    
+        
     // Handles all other keys, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
     } else {
         screen1Text += text;
         screen2Text += text;
     }
-
-
-
+    
+    // Display text on the screens
     screen1.textContent = screen1Text;
     screen2.textContent = screen2Text;
 }
